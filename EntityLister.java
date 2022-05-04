9783c26763ab6ef038ff7bdc15ebcd454b5a09e6
summisinha <102717604+summisinha@users.noreply.github.com>
2022-05-04 13:08:04 +0530

    Add files via upload

---
+++ EntityLister.java
@@ -0,0 +1,21 @@
+package com.project.SportyShoes.entity;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import java.util.Date;
+
+public class EntityLister {
+    @PrePersist
+    public void prePersist(BaseEntity entity) {
+        Date now = new Date();
+        entity.setCreateDate(now);
+    }
+
+    @PreUpdate
+    public void preUpdate(BaseEntity entity) {
+        Date now = new Date();
+        entity.setLastUpdateDate(now);
+    }
+}
+
---
+++ ProductRepository.java
@@ -0,0 +1,6 @@
+package com.project.SportyShoes.jdbc;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.SportyShoes.entity.Product;
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
